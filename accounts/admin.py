40214dc715b2473c7fe9cca51ec0c49b8c58cfa2
Cencious <user.email>
2022-12-09 14:40:07 +0300

    changes

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,5 +4,5 @@ from .models import Customer, Product, Order,Tag
 
 admin.site.register(Customer)
 admin.site.register(Product)
-admin.site.register(Order)
 admin.site.register(Tag)
+admin.site.register(Order)
