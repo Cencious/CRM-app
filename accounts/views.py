cdc1d14716c660ef79a97de1457f2045f84f4cce
Cencious <user.email>
2022-12-14 21:39:38 +0300

    pass in queries for total orders, total customers

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def home(request):
-    return render(request,'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+    total_customers =customers.count()
+    total_orders = orders.count()
+    delivered = orders.filter('Delivered').count()
+    pending = orders.filter('Pending').count()
+    context = {'orders':orders, 'customers':customers, 'pending':pending, 'delivered':delivered,'total_orders':total_orders}
+
+    return render(request,'accounts/dashboard.html', context)
 
 def products(request):
-    return render(request,'accounts/products.html')
+    products = Product.objects.all()
+
+    return render(request,'accounts/products.html',{'products':products})
 
 def customer(request):
+    # customers = Customer.objects.all()
     return render(request,'accounts/customer.html')
