a0cae1b90cd1f209f1aec4e9a23a079fe1d06530
Cencious <user.email>
2022-12-16 08:58:21 +0300

    Add email field to models

--- accounts/models.py
+++ accounts/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Customer(models.Model):
     name = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=200, null=True)
+    email = models.EmailField(max_length=200, null=True)
     date_created = models.DateField(auto_now_add=True, null=True)
 
     #allow see the actual name of customer
