ceb80411e51cc33cb26b2a8824b1ed854c054d95
Cencious <user.email>
2023-01-04 01:20:24 +0300

    create a create order function

--- accounts/views.py
+++ accounts/views.py
@@ -29,3 +29,8 @@ def customer(request, pk):
 
     context={'customers': customers, 'orders': orders, 'order_count': order_count}
     return render(request,'accounts/customer.html', context)
+
+def createOrder(request):
+    
+    context={}
+    return render(request,'accounts/order_form.html',context)
