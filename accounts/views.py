30102f427d2d3f01bc264256579371628d574fad
Cencious <user.email>
2023-01-07 05:57:23 +0300

    Save form and redirect to main dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
 from .forms import OrderForm
@@ -32,10 +32,14 @@ def customer(request, pk):
     return render(request,'accounts/customer.html', context)
 
 def createOrder(request):
-    form = OrderForm(request)
+    form = OrderForm()
     if request.method == 'POST':
-        print('printing POST:',request.POST)
-    form = OrderForm(request.POST)
+        # print('printing POST:',request.POST)
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save
+            return redirect('/')
+
     
     context={'form': form}
     return render(request,'accounts/order_form.html',context)
