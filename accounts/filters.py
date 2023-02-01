2cb79fd72360f0b9d2fdc6b50c3095d020c0e87e
Cencious <user.email>
2023-02-01 09:11:07 +0300

    Ajdust the FilterSet

--- accounts/filters.py
+++ accounts/filters.py
@@ -1,7 +1,7 @@
 import django_filters
 from .models import *
 
-class OrderFilter(django_filters.Filterset):
+class OrderFilter(django_filters.FilterSet):
     class Meta:
         model = Order
         fields = '__all__'
