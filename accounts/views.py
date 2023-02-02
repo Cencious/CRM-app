c85506d5234d534453e42f11a342bef608355507
Cencious <user.email>
2023-02-02 18:21:32 +0300

    process the form on views. send post data to db

--- accounts/views.py
+++ accounts/views.py
@@ -13,6 +13,11 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def registerPage(request):
     form =UserCreationForm()
+
+    if request.method == 'POST':
+        form =UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
     context ={'form':form}
     return render(request, 'accounts/register.html', context)
 
