d7ac142f6f8b3e61dcc529e33ec317a81691adf2
Cencious <user.email>
2023-02-07 12:54:09 +0300

    Add logout path

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # auth
     path('register/',views.registerPage, name='register'),
     path('login/',views.loginPage, name='login'),
+    path('logout/',views.logoutUser, name='logout'),
 
     # dashboard
     path('',views.home, name="home"),
