847b334dd849367f6bbea2a060545263d6620f36
Cencious <user.email>
2023-02-07 12:09:59 +0300

    from contrib.auth import auntenticate, login and logout to with all the three

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+
 from .models import *
 from .forms import OrderForm, CreateUserForm
 from .filters import OrderFilter
@@ -27,6 +29,7 @@ def registerPage(request):
     return render(request, 'accounts/register.html', context)
 
 def loginPage(request):
+    
     context ={}
     return render(request, 'accounts/login.html', context)
 
