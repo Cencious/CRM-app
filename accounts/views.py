2715dbf300e15f0a1caeb51c3890621597fe6574
Cencious <user.email>
2023-02-07 12:39:42 +0300

    Check if the user is there, and redirect to home

--- accounts/views.py
+++ accounts/views.py
@@ -35,7 +35,11 @@ def loginPage(request):
 
         user = authenticate(request, username= username, password= password)
 
-        
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+
+
     context ={}
     return render(request, 'accounts/login.html', context)
 
