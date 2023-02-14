6db3ccfc2b1c8f208704b5d5813777cc30f0f039
Cencious <kancencious@gmail.com>
2023-02-14 09:16:09 +0300

    Add extra functionality to original function

--- accounts/decorators.py
+++ accounts/decorators.py
@@ -3,12 +3,12 @@ from django.shortcuts import redirect
 
 
 #stop unauthenticated user from accessing the login and register page.
+#decorator is a function that takes in another function as apparator, allows adding an extra functionality before the original  function is called
 
 def unauthenticated_user(view_func):
     def wrapper_func(request, *args,**kwargs):
         if request.user.is_authenticated:
             return redirect('home')
         else:
-
             return view_func(request,*args, **kwargs)
-        return wrapper_func
+    return wrapper_func
