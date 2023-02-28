549b190b73010dbc6f3333661a512c6bf44c7348
Cencious <kancencious@gmail.com>
2023-02-28 10:44:29 +0300

    Create a form for the customer, to access details for the customer and update their fields

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,9 +2,16 @@ from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
+from .models import *
 
 
 from .models import Order
+
+class CustomerForm(ModelForm):
+    class Meta:
+        model = Customer
+        fields = '__All__'
+       
 
 
 class OrderForm(ModelForm):
