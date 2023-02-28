0e3a1a495c67e3cd22b0211e30879a64755b7afd
Cencious <kancencious@gmail.com>
2023-02-28 12:35:13 +0300

    adjust to customer variable and not user variable

--- accounts/views.py
+++ accounts/views.py
@@ -96,8 +96,8 @@ def userPage(request):
 @login_required(login_url='login')
 @allowed_users(allowed_roles=['customer'])
 def accountSettings(request):
-    user = request.user.customer #gets the current logged in user
-    form = CustomerForm(instance=user)
+    customer = request.user.customer #gets the current logged in user
+    form = CustomerForm(instance=customer)
     context ={'form': form}
     return render(request,'accounts/account_settings.html', context)
 
