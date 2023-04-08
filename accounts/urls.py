9e12406b57daf310876ad6da000c301c71fb9517
Cencious <kancencious@gmail.com>
2023-04-08 21:49:33 +0300

    specify the names

--- accounts/urls.py
+++ accounts/urls.py
@@ -22,8 +22,8 @@ urlpatterns = [
     path('delete_order/<str:pk>/', views.deleteOrder, name="delete_order"),
 
     # password reset Email
-    path('reset_password/', auth_views.PasswordResetView.as_view()),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view()),
-    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view()),
-    path('reset_password_complete/', auth_views.PasswordResetDoneView.as_view()),
+    path('reset_password/', auth_views.PasswordResetView.as_view(),name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetDoneView.as_view(), name="assword_reset_complete"),
 ]
