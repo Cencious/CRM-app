71af14b9d08c199f11f45288181e60a96c44747e
Cencious <kancencious@gmail.com>
2023-04-08 22:01:29 +0300

    set email backend

--- crm1/settings.py
+++ crm1/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 #SMTP Configuration
-EMAIL_BACKEND = ''
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = ''
 EMAIL_PORT = ''
 EMAIL_USE_TLS = ''
