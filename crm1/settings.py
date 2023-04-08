c2d79fdb35a30e8bd0f0995a447d1ef8c2e4dfe9
Cencious <kancencious@gmail.com>
2023-04-08 22:02:41 +0300

    set email host

--- crm1/settings.py
+++ crm1/settings.py
@@ -127,9 +127,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 #SMTP Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = ''
-EMAIL_PORT = ''
-EMAIL_USE_TLS = ''
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = 'True'
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD =''
 # Default primary key field type
