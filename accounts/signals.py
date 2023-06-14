b0a1e884de56fe87b2645badc8aee21ea1ac9c5c
Cencious <kancencious@gmail.com>
2023-06-14 10:05:49 +0300

    Create signals for customer profile

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,3 +1,14 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from .models import Customer
+
+
+def customer_profile(sender, instance, created, **kwargs):
+    if created:
+        group = Group.objects.get(name='Customer')
+        instance.groups.add(group)
+            
+        Customer.objects.create(
+            user = instance,
+            name=instance.usename
+            )
