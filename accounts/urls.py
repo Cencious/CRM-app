7c50f2f421fbea6ff47f4bbae21017e4f920177a
Cencious <kancencious@gmail.com>
2023-06-26 16:03:17 +0300

    Add the template view

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    
+
     # auth
     path('register/',views.registerPage, name='register'),
     path('login/',views.loginPage, name='login'),
@@ -23,7 +23,7 @@ urlpatterns = [
     path('delete_order/<str:pk>/', views.deleteOrder, name="delete_order"),
 
     # password reset Email
-    path('reset_password/', auth_views.PasswordResetView.as_view(),name="reset_password"),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"), name= "reset_password"),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
     path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
     path('reset_password_complete/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_complete"),
