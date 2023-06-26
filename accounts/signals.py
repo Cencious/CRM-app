c65f768bfb2abc1ff5c885665268f7ac86472c7b
Cencious <kancencious@gmail.com>
2023-06-26 12:58:15 +0300

    import the croup model coz user is added to  group

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,5 +1,7 @@
 from django.db.models.signals import post_save
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User 
+from django.contrib.auth.models import Group
+
 from .models import Customer
 
 
