81d8e4b33b3e3bbdeb736bc1ea6447712b9cc5f3
Cencious <kancencious@gmail.com>
2023-06-29 15:41:31 +0300

    changes

--- crm1/settings.py
+++ crm1/settings.py
@@ -126,17 +126,12 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 #SMTP Configuration- lets us know the email to be sent.
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS =True
-EMAIL_HOST_USER = 'kancencious@gmail.com'
-EMAIL_HOST_PASSWORD ='edithaseyo#*'
-# EMAIL_HOST = 'EMAIL_HOST'
-# EMAIL_PORT = 'EMAIL_PORT'
-# EMAIL_USE_TLS = 'EMAIL_USE_TLS'
-# EMAIL_HOST_USER = 'EMAIL_HOST_USER'
-# EMAIL_HOST_PASSWORD ='EMAIL_HOST_PASSWORD'
+
+EMAIL_HOST = 'EMAIL_HOST'
+EMAIL_PORT = 'EMAIL_PORT'
+EMAIL_USE_TLS = 'EMAIL_USE_TLS'
+EMAIL_HOST_USER = 'EMAIL_HOST_USER'
+EMAIL_HOST_PASSWORD ='EMAIL_HOST_PASSWORD'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
